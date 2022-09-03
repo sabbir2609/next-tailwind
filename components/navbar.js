65fbaf4be52740791d53a6e0a3c179ab394144b5
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2022-09-03 22:07:46 +0600

    fix res

--- components/navbar.js
+++ components/navbar.js
@@ -14,13 +14,9 @@ const Navbar = () => {
 
             <ul className="menu menu-horizontal p-0">
                 {/* <!-- Navbar menu content here --> */}
-
-                <li>
-                    <Link href="/about"><a>About</a></Link>
-                </li>
                 <li tabIndex="0">
                     <a>
-                        Parent
+                        <p className='hidden sm:block'>More</p>
                         <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                     </a>
                     <ul className="p-2 bg-base-100">
