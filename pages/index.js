e2722a68c882dc8322a574078f7cf37121eadc92
Sabbir Hasan Munna <sabbirhasan2999@gmail.com.com>
2023-01-06 00:48:16 +0600

    home update

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+
       <Head>
         <title>Homepage</title>
         <meta property="og:title" content="Homepage | sabbir inc" key="title" />
       </Head>
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image src="https://placeimg.com/260/400/arch" alt='Hero Image' width={200} height={800} className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
-          <h1 className="text-5xl font-bold">Sabbir Inc.</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
